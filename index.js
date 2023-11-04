5c8031657fa34b72d248c1b5be00ad9fe64a1f77
Anne Hastings <anne.miriam@gmail.com>
2023-11-03 21:08:42 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+// // Add your code here
+const baseUrl = 'http://localhost:3000/users';
+
+function submitData(name, email){
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        }),
+    })
+    .then(resp => resp.json())
+    .then((object) => {
+        document.body.innerHTML = object.id
+    })
+    .catch((error) => {
+        document.body.innerHTML = error.message
+    })
+}
+
